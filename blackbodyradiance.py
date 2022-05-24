26b85e60006c7a37d4c33ebd7f5785dc0b260c06
enemieltje <74739340+enemieltje@users.noreply.github.com>
2022-05-24 10:54:47 +0200

    added solar irradiance

--- blackbodyradiance.py
+++ blackbodyradiance.py
@@ -2,27 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-temp = 5000.0  # Kelvin
+solidAngle = 6.806E-6  # steradians
+temp = 5778.0  # Kelvin
 labda1 = 0.1  # micrometer
 labda2 = 2.0  # micrometer
-labdastep = 0.01  # mirometer
+labdastep = 0.01  # micrometer
 N = int(1.0 + (labda2 - labda1) / labdastep)
-blackbodyradiance = np.zeros(N)
+solarIrradiance = np.zeros(N)
 wavelength = np.zeros(N)
 
 
-def L(labda, temperature):  # blackbody radiance (W/(m2.sr.mu))
-	c1 = 1.191042E8  # (W/(m2.sr.mu^4))
-	c2 = 1.4387752E4  # (K.mu)
-	return c1 / (labda ** 5 * (math.exp(c2 / (labda * temperature)) - 1))  # Planck function for black body radiance
+def getSolarIrradiance(labda, temperature, solidAngle):  # solar irradiance
+	c1 = 1.191042E8  # (W/(m^2.sr.um^4))
+	c2 = 1.4387752E4  # (K.um)
+	
+	# Planck function for black body radiance (W/(m^2.sr.um))
+	planck = c1 / (labda ** 5 * (math.exp(c2 / (labda * temperature)) - 1))
+	
+	return planck / 1000 * solidAngle  # radiation at earth in nanometers (W/(m^2.nm))
 
 
 for i in range(0, N):
 	wavelength[i] = labda1 + i * labdastep
-	blackbodyradiance[i] = L(wavelength[i], temp)
+	solarIrradiance[i] = getSolarIrradiance(wavelength[i], temp, solidAngle)
 
 fig = plt.figure()
-plt.scatter(wavelength, blackbodyradiance)
-plt.ylabel('Black body radiance')
+plt.scatter(wavelength, solarIrradiance)
+plt.ylabel('Solar Irradiance')
 
-# plt.savefig('test.png')
+plt.savefig('test.png')
